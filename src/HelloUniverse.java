995bc2d484a30ae26514eaf348e2180942830956
Luong Michaël <luongmichael@macbook-air-de-luong-3.home>
2023-04-27 15:07:37 +0200

    add idea in gitignore and change Universe to World

---
+++ src/HelloUniverse.java
@@ -0,0 +1,10 @@
+public class HelloUniverse {
+    public static void main(String... args){
+        System.out.println("Hello World!");
+        int numberPlanet=8;
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : "+numberPlanet);
+        numberPlanet=9;
+        System.out.println("Il y a quelques années cependant, elles étaient au nombre de : "+numberPlanet);
+    }
+
+}
