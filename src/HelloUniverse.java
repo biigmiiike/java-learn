c80a9472a98626620001d2c1da035c7e37cc8a5f
Luong Michaël <luongmichael@macbook-air-de-luong-3.home>
2023-04-27 21:10:42 +0200

    Section4 Exo1 Bureau de change

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,10 +1,9 @@
 public class HelloUniverse {
     public static void main(String... args){
-        System.out.println("Hello World!");
-        int numberPlanet=8;
-        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : "+numberPlanet);
-        numberPlanet=9;
-        System.out.println("Il y a quelques années cependant, elles étaient au nombre de : "+numberPlanet);
-    }
+        short montantInit=1500;
+        float taux= 1.22F;
+        float conversion= montantInit*taux;
+        System.out.println(conversion);
+           }
 
 }
