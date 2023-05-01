66a6ee2a9e09065f9bfa957ed99aa790d09b2990
Luong Michaël <luongmichael@macbook-air-de-luong-3.home>
2023-05-01 17:28:56 +0200

    Exercice 5 codage secteur 4

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,13 +1,11 @@
 public class HelloUniverse {
     public static void main(String... args){
         String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(phrase);
         int nbPlanetes=8;
-        System.out.println(nbPlanetes);
+        System.out.println(phrase+nbPlanetes);
         phrase="Il y a quelques années cependant, elles étaient au nombre de : ";
-        System.out.println(phrase);
         nbPlanetes++;
-        System.out.println(nbPlanetes);
+        System.out.println(phrase+nbPlanetes);
            }
 
 }
