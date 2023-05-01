802ebeda96a0539210b1beb13cfa7f6716429d50
Luong Michaël <luongmichael@macbook-air-de-luong-3.home>
2023-05-01 17:21:44 +0200

    exercice de codage 4 secteur 4

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,9 +1,13 @@
 public class HelloUniverse {
     public static void main(String... args){
-        short montantInit=1500;
-        float taux= 1.22F;
-        float conversion= montantInit*taux;
-        System.out.println(conversion);
+        String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        System.out.println(phrase);
+        int nbPlanetes=8;
+        System.out.println(nbPlanetes);
+        phrase="Il y a quelques années cependant, elles étaient au nombre de : ";
+        System.out.println(phrase);
+        nbPlanetes++;
+        System.out.println(nbPlanetes);
            }
 
 }
