b370edd47de5086113b9aa25bddbbd87f1e2824f
Luong Michaël <luongmichael@macbook-air-de-luong-3.home>
2023-05-02 15:48:18 +0200

    Exercice de codage 8 section 5

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,11 +1,23 @@
 public class HelloUniverse {
-    public static void main(String... args){
-        int annee=2006;
-        if(annee<2006){
-            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : 9",annee);
+    public static void main(String... args) {
+        String phraseIntroduction="En %d, les planètes du système solaire étaient au nombre de : %d";
+        int nbPlanetes=0;
+        short annee=2021;
+        if (annee<1600 || annee>2020){
+            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d",annee);
+        }
+        else if (annee>=1600 && annee<1700){
+            nbPlanetes=7;
+            System.out.printf(phraseIntroduction,annee,nbPlanetes);
+        }
+        else if (annee>=1700 && annee<1800 || annee>=2006){
+            nbPlanetes=8;
+            System.out.printf(phraseIntroduction,annee,nbPlanetes);
         }
         else {
-            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : 8",annee);
+            nbPlanetes=9;
+            System.out.printf(phraseIntroduction,annee,nbPlanetes);
         }
+
     }
 }
